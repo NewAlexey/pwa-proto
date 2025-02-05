0ee7101529ab91a8aad11455ebad3a5738da76da
NewAlexey <alexey.krupenia@gmail.com>
2025-02-05 15:50:11 +0300

    fix: fix TS types

--- src/components/WebPushComponents.tsx
+++ src/components/WebPushComponents.tsx
@@ -11,34 +11,15 @@ const subscribeOnNotification = async () => {
 
         if (!("Notification" in window)) {
             alert("This browser does not support web push notification. This Demo has failed for you.  :'-( ");
-            document.getElementById("welcomemsg").innerHTML = "This browser does not support desktop notification. This Demo has failed :( ";
         } else {
             Notification.requestPermission(function (status) {
                 console.log('Notification Permissiong status:', status);
             });
 
             if (Notification.permission === 'denied') {
-                document.getElementById("welcomemsg").innerHTML = "You've denied notification on a notifcation DEMO! I'm sad!";
+                alert("You've denied notification on a notifcation DEMO! I'm sad!");
             } else {
-
-                // We are a go. Everything is ready.
-                // We've asked for notificaiton permissiongs. And we've been given it.
-                // Time to register our service worker to listen for notifications and pop them up when we receive them from the server.
-
-                // Register the serviceWorker script at /sw.js from our server if supported
                 if ('serviceWorker' in navigator) { // yes this browser has a serviceWorker functionality.
-                    // // First. Let's de-register any web workers we have for this domain just in case damian was testing things.
-                    // // 1) Get all registered ServiceWorkers
-                    // navigator.serviceWorker.getRegistrations().then((registrations) => {
-                    //     // loop through them and un-register from them.
-                    //     for (let registration of registrations) {
-                    //         registration.unregister()
-                    //     }
-                    // }).catch(function (err) {
-                    //     console.log('Service Worker Unregistration failed: ', err);
-                    // });
-
-
                     try {
                         const register = await navigator.serviceWorker.register('./sw.js', { scope: '/' });
                         const subscription = await register.pushManager.subscribe({
@@ -53,7 +34,7 @@ const subscribeOnNotification = async () => {
             } // end of if (Notification.permission === 'denied' )
         } // end of - if (!("Notification" in window))
 
-    function PostSubscriptionDetails(Subscription) {
+    function PostSubscriptionDetails(Subscription: PushSubscription) {
         // let's parse the details of the subscription we got back from the creator of this browser
         let sub = JSON.parse(JSON.stringify(Subscription));
         let token = sub.keys.p256dh;
